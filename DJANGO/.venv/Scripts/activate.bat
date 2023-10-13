f190953680166f59e20545ab89337041b724db9d
Fermin921 <ferminmartinez921@gmail.com>
2023-10-13 09:52:52 -0500

    Actualizacion Datos

--- DJANGO/.venv/Scripts/activate.bat
+++ DJANGO/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\fer_m\OneDrive\Escritorio\TEC\Multiparadigma\DJANGO\.venv
+set VIRTUAL_ENV=C:\Users\fer_m\OneDrive\Escritorio\TEC\Multiparadigma\Multiparadigma-Materia\Multiparadigma\DJANGO\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
