6541b017c0d84ae67ba4e1744b8c069636c59f8b
Fermin921 <ferminmartinez921@gmail.com>
2023-10-18 20:56:08 -0500

    Miercoles

--- Flask2/app.py
+++ Flask2/app.py
@@ -3,9 +3,12 @@ from database import db
 from config import BasicConfig
 from flask_migrate import Migrate
 import logging
+from Routes.persona.personas import appersona
 
 app = Flask(__name__)
+app.register_blueprint(appersona)
 app.config.from_object(BasicConfig)
 db.init_app(app)
 migrate = Migrate()
 migrate.init_app(app, db)
+logging.basicConfig(level=logging.DEBUG, filename="logs.log")
